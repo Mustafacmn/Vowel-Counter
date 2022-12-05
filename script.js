f44e0badd547c6a92d40f9cea700be9886a8769a
Mustafa <mustafamcm9@gmail.com>
2022-12-05 03:09:21 +0300

    (new commit)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inputEl = document.getElementById("input")
 const checkBtn = document.getElementById("check-btn");
 
-const vowelRegex = /[aeiou]/gi
+const vowelRegex = /[aeiou]/gi // Check for vowels
 
 checkBtn.addEventListener("click", () => {
   const input = inputEl.value
